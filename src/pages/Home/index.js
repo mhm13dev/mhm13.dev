227a5779c510b5cecf0ff0fa3aa59a4965628dd6
Mubashir Hassan <official.mhm13@gmail.com>
2021-07-06 05:23:02 +0500

    Added: Social Media Icons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import MubashirProfile from '../../assets/images/mubashir.png';
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+} from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -17,10 +24,47 @@ const Home = () => {
           <div className="text-center mt-4">
             <Link
               to="/"
-              className="text-3xl font-bold font-nunito inline-block"
+              className="text-2xl font-semibold font-nunito inline-block"
             >
-              mhm13.dev
+              Mubashir Hassan | mhm13.dev
             </Link>
+          </div>
+          <div className="text-center mt-4 flex gap-x-4 items-center justify-center">
+            <a
+              href="https://github.com/mhm13dev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub className="w-6 h-6" />
+            </a>
+            <a
+              href="https://twitter.com/mhm13dev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter className="w-6 h-6" />
+            </a>
+            <a
+              href="https://instagram.com/mhm13dev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram className="w-6 h-6" />
+            </a>
+            <a
+              href="https://facebook.com/mhm13dev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mhm13dev"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin className="w-6 h-6" />
+            </a>
           </div>
         </div>
       </div>
