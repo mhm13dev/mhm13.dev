3464bb242c4d1c78f805aa1395ef6d70c442258c
Mubashir Hassan <official.mhm13@gmail.com>
2023-09-22 00:44:58 +0500

    feat: redirect /resume to google drive resume as rewrite is not displaying resume in production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => [
+  redirects: async () => [
     {
       source: "/resume",
       destination:
         "https://drive.google.com/file/d/1coxDlJyoWRS3iPNs-XcwVNlNgqJzWKw9/view",
+      permanent: true,
     },
   ],
 };
